62be267422afaa3a883043a0c166500c637f6696
Adi001100 <114089548+Adi001100@users.noreply.github.com>
2024-12-03 14:28:05 +0100

    Auto-generated commit

--- main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-YY6Z3XBB.mjs";import"./chunk-DJQTDTIA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-K45L27XX.mjs";import"./chunk-DJQTDTIA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
